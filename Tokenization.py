3743c47b0ef8f5f94f8872a7dceb7ec6ac6d4558
Nilkrishna <72926230+Nilkrishna@users.noreply.github.com>
2020-10-15 15:42:03 +0200

    Add files via upload

---
+++ Tokenization.py
@@ -0,0 +1,17 @@
+
+
+# Tokenization of paragraphs/sentences
+import nltk
+nltk.download()
+
+          
+# Tokenizing sentences
+sentences = nltk.sent_tokenize(paragraph)
+
+# Tokenizing words
+words = nltk.word_tokenize(paragraph)
+
+
+
+
+
